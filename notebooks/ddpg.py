f5728b5812acc137acdbf340dd86bed63db7fb62
Andrew Hart <AndrewHartAR@users.noreply.github.com>
2026-02-05 16:17:22 +0000

    adds ddpg

--- notebooks/ddpg.py
+++ notebooks/ddpg.py
@@ -15,12 +15,9 @@
 # ---
 
 # %% [markdown]
-# PPO, Lunar Lander Continuous
+# DDPG, Lunar Lander Continuous
 #
 # This implementation matches SB3-Zoo.
-#
-# Not supported, from SB3-Zoo implementation:
-# - vf_coef, we have separate actor/critic so not relevant
 
 # %%
 import torch
@@ -36,28 +33,37 @@ np.random.seed(seed)
 
 env_name = 'LunarLander-v3'
 continuous = True
-n_timesteps = 400000
+n_timesteps = 100000
 
 env = gym.make(env_name, continuous=continuous)
 state_dim = env.observation_space.shape[0]
 action_dim = env.action_space.shape[0]
 action_range = torch.tensor(np.stack([env.action_space.low, env.action_space.high]))
 
-actor_params = DistributionalActorParams(
-    policy_objective_method=PolicyObjectiveMethodClipped(clip_range=0.2),
-    entropy_coef=0.01
+actor_params = DeterministicActorParams(
+    exploration_std=0.1
 )
-actor = StochasticActor(state_dim=state_dim, action_dim=action_dim, action_range=action_range, params=actor_params)
-critic = Critic(state_dim=state_dim)
-agent = Agent(actor=actor, critics=[critic])
+actor = DeterministicActor(
+    state_dim=state_dim, action_dim=action_dim, action_range=action_range,
+    hidden_sizes=[400, 300], params=actor_params)
+
+critic_params = CriticParams(critic_loss_method=CriticLossMethodQ())
+critic = QCritic(state_dim=state_dim, action_dim=action_dim, hidden_sizes=[400, 300], params=critic_params)
+
+agent_params = AgentWithTargetsParams(
+    tau=0.005
+)
+agent = AgentWithTargets(actor=actor, critics=[critic], params=agent_params)
+
 params = Params(
-    rollout_method=RolloutMethodA2C(n_steps=1024, n_envs=16),
-    gradient_transform=GradientTransformClipNorm(max_norm=0.5),
+    rollout_method=RolloutMethodA2C(n_steps=1, n_envs=1),
     advantage_method=AdvantageMethodGAE(lambda_=0.98),
     advantage_transform=AdvantageTransformNormalize(),
-    data_load_method=DataLoadMethodEpochs(n_epochs=4, mb_size=64),
-    lr_schedule=LRScheduleLinearAnneal(start_lr=0.0003, end_lr=0.0),
-    gamma=0.999
+    data_load_method=DataLoadMethodReplay(
+        capacity=200000, state_dim=state_dim, action_dim=action_dim, batch_size=256, minimum_size=10000),
+    gradient_transform=GradientTransformClipNorm(max_norm=0.5),
+    lr_schedule=LRScheduleConstant(lr=0.001),
+    gamma=0.98
 )
 
 returns, lengths = trainer.train(agent, env_name, continuous, params, n_timesteps, seed=seed)
@@ -65,7 +71,7 @@ returns, lengths = trainer.train(agent, env_name, continuous, params, n_timestep
 # %%
 # show plot
 plot.plot_session(
-    returns, lengths, 'PPO', env_name, continuous=continuous,
+    returns, lengths, 'DDPG', env_name, continuous=continuous,
     n_timesteps=n_timesteps, agent=agent, params=params)
 
 # %% [markdown]
@@ -107,21 +113,30 @@ def standard():
     action_dim = env.action_space.shape[0]
     action_range = torch.tensor(np.stack([env.action_space.low, env.action_space.high]))
 
-    actor_params = DistributionalActorParams(
-        policy_objective_method=PolicyObjectiveMethodClipped(clip_range=0.2),
-        entropy_coef=0.01
+    actor_params = DeterministicActorParams(
+        exploration_std=0.1
     )
-    actor = StochasticActor(state_dim=state_dim, action_dim=action_dim, action_range=action_range, params=actor_params)
-    critic = Critic(state_dim=state_dim)
-    agent = Agent(actor=actor, critics=[critic])
+    actor = DeterministicActor(
+        state_dim=state_dim, action_dim=action_dim, action_range=action_range,
+        hidden_sizes=[400, 300], params=actor_params)
+
+    critic_params = CriticParams(critic_loss_method=CriticLossMethodQ())
+    critic = QCritic(state_dim=state_dim, action_dim=action_dim, hidden_sizes=[400, 300], params=critic_params)
+
+    agent_params = AgentWithTargetsParams(
+        tau=0.005
+    )
+    agent = AgentWithTargets(actor=actor, critics=[critic], params=agent_params)
+
     params = Params(
-        rollout_method=RolloutMethodA2C(n_steps=1024, n_envs=16),
-        gradient_transform=GradientTransformClipNorm(max_norm=0.5),
+        rollout_method=RolloutMethodA2C(n_steps=1, n_envs=1),
         advantage_method=AdvantageMethodGAE(lambda_=0.98),
         advantage_transform=AdvantageTransformNormalize(),
-        data_load_method=DataLoadMethodEpochs(n_epochs=4, mb_size=64),
-        lr_schedule=LRScheduleLinearAnneal(start_lr=0.0003, end_lr=0.0),
-        gamma=0.999
+        data_load_method=DataLoadMethodReplay(
+            capacity=200000, state_dim=state_dim, action_dim=action_dim, batch_size=256, minimum_size=10000),
+        gradient_transform=GradientTransformClipNorm(max_norm=0.5),
+        lr_schedule=LRScheduleConstant(lr=0.001),
+        gamma=0.98
     )
 
     return agent, env_name, continuous, params
@@ -129,11 +144,11 @@ def standard():
 
 # %%
 n_sessions = 16
-n_timesteps = 400000
+n_timesteps = 100000
 timeout = 3600 # 1 hour
 setup_func = standard
 
 results = modal_training.train(n_sessions, n_timesteps, setup_func, app, image, timeout=timeout)
 
 # %%
-modal_training.plot_results(results, 'PPO', n_timesteps)
+modal_training.plot_results(results, 'DDPG', n_timesteps)
